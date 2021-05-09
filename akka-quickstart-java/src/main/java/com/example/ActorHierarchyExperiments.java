c9e7ec222a00f7d3b60bfb033a7231573b95f903
Ken Mau <ken_mau@hotmail.com>
2021-05-09 10:27:24 -0400

    commit next akka example

---
+++ akka-quickstart-java/src/main/java/com/example/ActorHierarchyExperiments.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import akka.actor.ActorSystem;
+import akka.actor.typed.ActorRef;
+
+public class ActorHierarchyExperiments {
+    public static void main(String[] args) {
+        ActorRef<String> testSystem = ActorSystem.create(Main.create(), "testSystem");
+        testSystem.tell("start");
+    }
+}
